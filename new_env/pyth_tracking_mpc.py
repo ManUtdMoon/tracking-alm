d24bf4b108ad0548e112851d6d940dbcedc4c30d
ManUtdMoon <yudongjie.moon@foxmail.com>
2023-12-18 16:43:41 +0800

    rollout and cost calculation in batch

--- new_env/pyth_tracking_mpc.py
+++ new_env/pyth_tracking_mpc.py
@@ -25,11 +25,35 @@ class ModelPredictiveController(object):
         self.tunable_para_unmapped = torch.tensor([-128916, -85944, 1.06, 1.85, 1412, 1536.7], dtype=torch.float32)
         self.Q_mat = torch.diag(torch.tensor([1.0, 1e-5, 1., 10., 30., 50.], dtype=torch.float32))
         self.R_mat = torch.diag(torch.tensor([60., 1.], dtype=torch.float32))
+        self.Q_vec = torch.tensor([1.0, 1e-5, 1., 10., 30., 50.], dtype=torch.float32)
+        self.R_vec = torch.tensor([60., 1.], dtype=torch.float32)
         # self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.x0 = torch.rand(8 * self.Np, dtype=torch.float32, requires_grad=True)
         self.dim = len(self.x0)
         self.ref_p = None
         self.gamma = 0.99
+
+    def batch_step_forward(self, states, actions):
+        assert states.shape[0] == actions.shape[0]
+        assert states.shape[1] == 6
+        assert actions.shape[1] == 2
+        x, v_x, v_y, r, y, phi = states[:, 0], states[:, 1], states[:, 2], states[:, 3], states[:, 4], states[:, 5]
+        steer, a_x = actions[:, 0], actions[:, 1]
+        C_f, C_r, a, b, mass, I_z = self.tunable_para_unmapped[:6].tolist()
+        tau = self.step_T
+
+        next_x = x + tau * (v_x * torch.cos(phi) - v_y * torch.sin(phi))
+        next_u = v_x + tau * a_x
+        next_v = (mass * v_y * v_x + tau * (a * C_f - b * C_r) * r -
+                 tau * C_f * steer * v_x - tau * mass * (v_x ** 2) * r) / (mass * v_x - tau * (C_f + C_r))
+        next_w = (I_z * r * v_x + tau * (a * C_f - b * C_r) * v_y
+                 - tau * a * C_f * steer * v_x) / (I_z * v_x - tau * ((a ** 2) * C_f + (b ** 2) * C_r))
+        next_y = y + tau * (v_x * torch.sin(phi) + v_y * torch.cos(phi))
+        next_phi = phi + tau * r
+
+        next_states = torch.stack([next_x, next_u, next_v, next_w, next_y, next_phi], dim=1)
+
+        return next_states
 
     def step_forward(self, state, action):
         x, v_x, v_y, r, y, phi = state[0], state[1], state[2], state[3], state[4], state[5]
@@ -48,6 +72,51 @@ class ModelPredictiveController(object):
                       phi + tau * r])
         return next_state
     
+    def compute_cost_batch(self, x):
+        cost = torch.tensor(0., dtype=torch.float32)
+        x_init = torch.tensor(self.initial_state[:6], dtype=torch.float32)
+        x = x.reshape(-1, 8)
+        actions = x[:, 0:2]
+        next_states_variable = x[:, 2:] # (Np, 6) x_1~x_Np
+        discount = torch.tensor([self.gamma**i for i in range(1, 1 + self.Np)], dtype=torch.float32)
+
+        # tracking cost
+        ref_x = torch.as_tensor(self.initial_state[6::3], dtype=torch.float32) # x
+        ref_y = torch.as_tensor(self.initial_state[7::3], dtype=torch.float32) # y
+        ref_phi = torch.as_tensor(self.initial_state[8::3], dtype=torch.float32) # phi
+        ref = torch.stack(
+            [
+                ref_x, # x
+                self.u_target * torch.ones_like(ref_x, dtype=torch.float32), # u
+                torch.zeros_like(ref_x, dtype=torch.float32), # v
+                torch.zeros_like(ref_x, dtype=torch.float32), # w
+                ref_y, # y
+                ref_phi, # phi
+            ],
+            dim=1
+        )
+        assert ref.shape == next_states_variable.shape
+        tracking_error = next_states_variable - ref # (Np, 6)
+        tracking_cost = torch.sum(torch.sum(tracking_error * tracking_error * self.Q_vec, dim=1) * discount) # scalar
+
+        # action cost
+        action_cost = torch.sum(torch.sum(actions * actions * self.R_vec, dim=1) * discount) # scalar
+        cost += tracking_cost + action_cost
+
+        # dynamics constraints
+        current_states = torch.cat((x_init.reshape(1, -1), x[:-1, 2:]), dim=0) # (Np, 6) x_0~x_(Np-1)
+        next_states_rollout = self.batch_step_forward(current_states, actions) # (Np, 6) x_1~x_Np
+        state_deviation = (next_states_rollout - next_states_variable).reshape(-1) # (Np * 6,)
+        cost += torch.dot(self.lam, state_deviation) + self.rho / 2. * torch.linalg.norm(state_deviation, 1) ** 2
+
+        # action bound constraints
+        violation_lb = torch.max(self.action_lb - actions, torch.zeros_like(actions, dtype=torch.float32)).reshape(-1)
+        violation_ub = torch.max(actions - self.action_ub, torch.zeros_like(actions, dtype=torch.float32)).reshape(-1)
+        cost += torch.dot(self.lam_lb, violation_lb) + self.rho / 2. * torch.linalg.norm(violation_lb, 1) ** 2
+        cost += torch.dot(self.lam_ub, violation_ub) + self.rho / 2. * torch.linalg.norm(violation_ub, 1) ** 2
+
+        return cost
+
     def compute_cost(self, x):
         cost = torch.tensor(0., dtype=torch.float32)
         # optimized vector: u0, x1, u1, ..., xn-1, un-1, xn
@@ -389,26 +458,23 @@ class ModelPredictiveController(object):
             max_iteration = 500
             for iter in range(max_iteration):
                 # compute newton step and decrement
-                cost = self.compute_cost(self.x0)
-                g = torch.autograd.grad(cost, self.x0, retain_graph=True, create_graph=True)
-                # H = torch.zeros((self.dim, self.dim), dtype=torch.float32, requires_grad=False)
-                # for i in range(self.dim):
-                #     grad_item = g[0][i]
-                #     H[i] = torch.autograd.grad(grad_item, self.x0, retain_graph=True)[0]
-                # inv_H = torch.linalg.inv(H)
+                cost = self.compute_cost_batch(self.x0)
+                g = torch.autograd.grad(cost, self.x0, retain_graph=True, create_graph=True)[0]
+                H = torch.autograd.functional.hessian(self.compute_cost_batch, self.x0)
+                inv_H = torch.linalg.inv(H)
                 with torch.no_grad():
-                    # delta = - torch.matmul(inv_H, g[0])
-                    # lam_sq = - torch.matmul(g[0], delta)
-                    delta = -g[0]
-                    norm = torch.linalg.norm(delta)
-                    print(norm)
-                if norm < 1e-5:
+                    delta = - torch.matmul(inv_H, g)
+                    lam_sq = - torch.matmul(g, delta)
+                    # delta = -g[0]
+                    # norm = torch.linalg.norm(delta)
+                    print(lam_sq)
+                if lam_sq < 1e-5:
                     break
                 # backtracking line search
                 alpha, beta = 0.25, 0.8
                 t = 1
                 with torch.no_grad():
-                    while self.compute_cost(self.x0+t*delta) > cost+alpha*t*torch.dot(g[0],delta):
+                    while self.compute_cost_batch(self.x0 + t * delta) > cost+alpha*t*torch.dot(g, delta):
                         t *= beta
                 self.x0 = self.x0 + t * delta
                 print(self.x0[:2])
