84a530be4df4f61b6d144ab3f40efab3fd6d1ca8
ManUtdMoon <yudongjie.moon@foxmail.com>
2023-12-18 15:29:15 +0800

    improve plot codes

--- plot_opt.py
+++ plot_opt.py
@@ -38,6 +38,7 @@ print(alm_process["obj_aug_term"])
 # plt.savefig("./plot/obj.pdf")
 # plt.show()
 
+
 # # violation
 # plt.figure(figsize=(5, 2))
 # plt.plot(iterations, alm_process["max_violation_lb"], 'r-', label="Action lb violation")
@@ -46,7 +47,7 @@ print(alm_process["obj_aug_term"])
 # plt.ylabel("Violation")
 # plt.legend()
 # plt.tight_layout()
-# plt.savefig("./plot/dyn_violation.pdf")
+# plt.savefig("./plot/act_violation.pdf")
 
 
 # # Plot dynamics violation on the right subplot with log-y axis
@@ -57,24 +58,57 @@ print(alm_process["obj_aug_term"])
 # plt.legend()
 
 # plt.tight_layout()
-# plt.savefig("./plot/act_violation.pdf")
+# plt.savefig("./plot/dyn_violation.pdf")
 # plt.show()
 
 
-# plot the trajectory of ego car and reference trajectory
+# # plot the trajectory of ego car and reference trajectory
+# plt.figure(figsize=(5, 2))
+# ref_x = np.array([d["x_ref"][0] for d in exp])
+# ref_y = np.array([d["x_ref"][1] for d in exp])
+
+# ego_x = np.array([d["x"][0] for d in exp])
+# ego_y = np.array([d["x"][4] for d in exp])
+# plt.plot(ref_x, ref_y, 'r--', label="reference trajectory")
+# plt.plot(ego_x, ego_y, 'k-', label="ego trajectory")
+
+# plt.legend()
+# plt.xlabel("x [m]")
+# plt.ylabel("y [m]")
+# plt.ylim([-12, 10])
+# plt.tight_layout()
+# plt.savefig("./plot/traj.pdf")
+# plt.show()
+
+# # plot the action
+# delta = np.array([d["u"][0] for d in exp])
+# a = np.array([d["u"][1] for d in exp])
+# fig, ax1 = plt.subplots(figsize=(5, 2))
+
+# steps = np.arange(1, len(delta)+1)
+
+# ax1.set_xlabel('Time step')
+# ax1.set_ylabel('Steering angle [rad]', color="r")
+# ax1.plot(steps, delta, "r-")
+# ax1.tick_params(axis='y', labelcolor='r')
+
+# ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+# ax2.set_ylabel(r'Acceleration [m/s$^2$]', color="b")  # we already handled the x-label with ax1
+# ax2.plot(steps, a, "b--")
+# ax2.tick_params(axis='y', labelcolor="b")
+
+# fig.tight_layout()  # otherwise the right y-label is slightly clipped
+# plt.savefig("./plot/action.pdf")
+# plt.show()
+
+
+# plot the iterations of each step
 plt.figure(figsize=(5, 2))
-ref_x = np.array([d["x_ref"][0] for d in exp])
-ref_y = np.array([d["x_ref"][1] for d in exp])
-
-ego_x = np.array([d["x"][0] for d in exp])
-ego_y = np.array([d["x"][4] for d in exp])
-plt.plot(ref_x, ref_y, 'r--', label="reference trajectory")
-plt.plot(ego_x, ego_y, 'k-', label="ego trajectory")
-
-plt.legend()
-plt.xlabel("x [m]")
-plt.ylabel("y [m]")
-plt.ylim([-12, 10])
+steps = np.arange(1, len(exp)+1)
+plt.plot(steps, [d["num_iteration"] - 1 for d in exp], "k-")
+plt.xlabel("Time step")
+plt.ylabel("Number of iterations")
 plt.tight_layout()
-plt.savefig("./plot/traj.pdf")
+plt.savefig("./plot/iterations.pdf")
 plt.show()
